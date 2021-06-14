7b7861e3e486a055550796c32cd4a8f2a902be55
Pragya <prag05jaiswal@gmail.com>
2021-06-14 07:53:15 +0530

    cin_property

---
+++ character array/cin_property.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char input[100];
+    cin>>input;
+    cout<<input<<endl;
+}
+//cin function stop taking a input whenever we press space , tab .and newline;
