26e20d26abd5668d90f0e3c022f203c00fc1bef6
Pragya <prag05jaiswal@gmail.com>
2021-06-14 12:18:29 +0530

    print_prefix

---
+++ character array/print_all_the_prefix.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void printAllPrefix(char input[]){
+    for(int i = 0 ; input[i] !='\0' ; i++){
+        for(int j = 0 ; j <=i ; j++){
+            cout<<input[j];
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    char input[100] ="abcd";
+    printAllPrefix(input);
+
+}
